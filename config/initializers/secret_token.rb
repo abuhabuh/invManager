b02b535a33fd1ff0cb18041549124b0159f31d80
John Wang <jue@alumni.cmu.edu>
2013-11-10 13:01:27 -0800

    Intial commit of empty app shell for github.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InvManager::Application.config.secret_key_base = 'fdc7f64591b6635380a3df6dcdc097318a5cbbd3c8eeac89484fa819ff2e4e39b88c3b83ece084b5de7e41f31693649978ac0c0add8b53139af975c1ac3457f4'
